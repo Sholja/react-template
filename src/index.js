3bb4430c4328e580b6153a7406291b52c0fb6e4c
Eldin Soljic <eldin.soljic@ministryofprogramming.com>
2019-12-29 20:55:46 +0100

    Added wrapper for translations

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import I18n from 'redux-i18n';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
 import { configureStore } from './core/store';
 import App from './App';
 import initilizeHttpClient from './core/axios-config';
+import { translations } from './core/translations';
 
 initilizeHttpClient();
 
@@ -15,9 +17,11 @@ export const { persistor, store } = configureStore();
 ReactDOM.render(
   <PersistGate loading={null} persistor={persistor}>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <I18n translations={translations}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </I18n>
     </Provider>
   </PersistGate>,
   document.getElementById('root'),
