f02e79f549644e70c40b9bfc9719b489c622155e
Eldin Soljic <soljic.eldin@gmail.com>
2023-01-18 10:07:37 +0100

    Small adjustments to eslint (#59)
    Co-authored-by: Eldin Soljic <eldin.soljic@ministryofprogramming.com>

--- src/api/requests/users-api.js
+++ src/api/requests/users-api.js
@@ -1,9 +1,8 @@
 import { formAxiosObject } from '../../common/helpers';
 import { LOGIN } from '../request-objects/users-requests';
 
-export const loginApi = (email, password) => {
-  return formAxiosObject(LOGIN, {
+export const loginApi = (email, password) =>
+  formAxiosObject(LOGIN, {
     email,
     password,
   });
-}
